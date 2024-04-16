a2be3100e54b4c13087818032cad1a02cf18d483
Venuazmeera <venuazmeera69@gmail.com>
2024-04-16 20:51:20 +0530

    added q for offline users

--- index.js
+++ index.js
@@ -309,60 +309,158 @@
 //      } )
 // });
 
+// import express from 'express';
+// import { createServer } from 'http';
+// import WebSocket, { WebSocketServer } from "ws";
+// import { parse } from "url";
+// import { v4 as uuidv4 } from "uuid";
+
+
+// const app = express();
+// const server = createServer(app);
+
+// // const wss = new WebSocketServer({ port: 3006 });
+// const wss = new WebSocketServer({ noServer: true });
+
+// // Object to store WebSocket connections keyed by userId
+// const clients = {};
+
+// //for testing cyclic or vercel or heroku cloud
+// app.get("/", (req, res, next)=>{
+//     res.json({
+//         name:"chat-app",
+//         message:"i am working"
+//     });
+// });
+
+// wss.on("connection", function (ws, req) {
+//     console.log("Connection initiated");
+
+//     // Parse the URL to extract user ID
+//     const parsedUrl = parse(req.url, true);
+//     const userId = decodeURIComponent(parsedUrl.query.id) || uuidv4();
+//     console.log("User ID:", userId);
+//     ws.id = userId; // Assign the userId to the WebSocket connection
+
+//     // Store the WebSocket connection in the clients object
+//     clients[userId] = ws;
+
+//     ws.on("message", function message(msg) {
+//         const data = JSON.parse(msg);
+//         console.log(data);
+//         console.log("Received message from", ws.id);
+
+//         // Check if the message is intended for a specific user
+//         if (data.type === "message" && data.receiverId) {
+//             const receiverId = data.receiverId;
+//             const targetClient = clients[receiverId];
+
+//             // If the target user is found and the WebSocket is open, send the message
+//             if (targetClient && targetClient.readyState === WebSocket.OPEN) {
+//                 targetClient.send(JSON.stringify({ type: "message", data: data.data, senderUserId: ws.id, senderName: data.senderName }));
+
+//             } else {
+//                 console.log("No client found with the receiverId:", receiverId);
+//                 // Optionally, send an error message back to the sender
+//                 ws.send(JSON.stringify({ type: "error", message: "Receiver not found or connection closed." }));
+//             }
+//         } else {
+//             console.log("Message type not supported or receiverId missing.");
+//         }
+//     });
+
+//     ws.on("close", function close() {
+//         console.log("Connection closed for user:", ws.id);
+//         // Remove the WebSocket connection from the clients object
+//         delete clients[ws.id];
+//     });
+// });
+
+// // Handle upgrade of the request
+// server.on('upgrade', function upgrade(request, socket, head) {
+//     const { pathname } = parse(request.url);
+
+//     if (pathname === '/') {
+//         wss.handleUpgrade(request, socket, head, function done(ws) {
+//             wss.emit('connection', ws, request);
+//         });
+//     } else {
+//         socket.destroy();
+//     }
+// });
+
+// setInterval(() => {
+//     wss.clients.forEach((client) => {
+//       client.send(new Date().toTimeString());
+//     });
+//   }, 10000);
+
+// // Start the server
+// const PORT = process.env.PORT || 3006;
+// server.listen(PORT, function() {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+// // console.log(`WebSocket server is running on port 3006.`);
 import express from 'express';
 import { createServer } from 'http';
 import WebSocket, { WebSocketServer } from "ws";
 import { parse } from "url";
 import { v4 as uuidv4 } from "uuid";
 
-
 const app = express();
 const server = createServer(app);
-
-// const wss = new WebSocketServer({ port: 3006 });
 const wss = new WebSocketServer({ noServer: true });
 
 // Object to store WebSocket connections keyed by userId
 const clients = {};
 
-//for testing cyclic or vercel or heroku cloud
-app.get("/", (req, res, next)=>{
+// Object to store messages for offline users
+const messageQueue = {};
+
+app.get("/", (req, res, next) => {
     res.json({
-        name:"chat-app",
-        message:"i am working"
+        name: "chat-app",
+        message: "i am working"
     });
 });
 
 wss.on("connection", function (ws, req) {
     console.log("Connection initiated");
 
-    // Parse the URL to extract user ID
     const parsedUrl = parse(req.url, true);
     const userId = decodeURIComponent(parsedUrl.query.id) || uuidv4();
     console.log("User ID:", userId);
-    ws.id = userId; // Assign the userId to the WebSocket connection
+    ws.id = userId;
 
-    // Store the WebSocket connection in the clients object
     clients[userId] = ws;
+
+    // Check for and send any messages that were queued for this user
+    if (messageQueue[userId]) {
+        messageQueue[userId].forEach((queuedMessage) => {
+            ws.send(JSON.stringify(queuedMessage));
+        });
+        delete messageQueue[userId]; // Clear the message queue for this user
+    }
 
     ws.on("message", function message(msg) {
         const data = JSON.parse(msg);
         console.log(data);
         console.log("Received message from", ws.id);
 
-        // Check if the message is intended for a specific user
         if (data.type === "message" && data.receiverId) {
             const receiverId = data.receiverId;
             const targetClient = clients[receiverId];
 
-            // If the target user is found and the WebSocket is open, send the message
             if (targetClient && targetClient.readyState === WebSocket.OPEN) {
                 targetClient.send(JSON.stringify({ type: "message", data: data.data, senderUserId: ws.id, senderName: data.senderName }));
-
             } else {
-                console.log("No client found with the receiverId:", receiverId);
-                // Optionally, send an error message back to the sender
-                ws.send(JSON.stringify({ type: "error", message: "Receiver not found or connection closed." }));
+                console.log("Receiver is offline, queuing message");
+                // Store the message in the queue for the receiver
+                if (!messageQueue[receiverId]) {
+                    messageQueue[receiverId] = [];
+                }
+                messageQueue[receiverId].push({ type: "message", data: data.data, senderUserId: ws.id, senderName: data.senderName });
             }
         } else {
             console.log("Message type not supported or receiverId missing.");
@@ -371,12 +469,10 @@ wss.on("connection", function (ws, req) {
 
     ws.on("close", function close() {
         console.log("Connection closed for user:", ws.id);
-        // Remove the WebSocket connection from the clients object
         delete clients[ws.id];
     });
 });
 
-// Handle upgrade of the request
 server.on('upgrade', function upgrade(request, socket, head) {
     const { pathname } = parse(request.url);
 
@@ -391,14 +487,11 @@ server.on('upgrade', function upgrade(request, socket, head) {
 
 setInterval(() => {
     wss.clients.forEach((client) => {
-      client.send(new Date().toTimeString());
+        client.send(new Date().toTimeString());
     });
-  }, 10000);
+}, 10000);
 
-// Start the server
 const PORT = process.env.PORT || 3006;
-server.listen(PORT, function() {
+server.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}`);
 });
-
-// console.log(`WebSocket server is running on port 3006.`);
